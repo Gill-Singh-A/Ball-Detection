3565e587341075599024be9e58246fb912ad0645
Gill-Singh-A <amansg22@iitk.ac.in>
2023-09-26 18:37:12 +0530

    Improved Ball Detection

--- ball_detection.py
+++ ball_detection.py
@@ -1,22 +1,70 @@
 #!/usr/bin/env python3
 
-import cv2, numpy                                                                                                                          # Importing cv2 for image processing and numpy for mathematical operations
+import cv2, numpy, math
 
-if __name__ == "__main__":                                                                                                                 # If this program is the main namespace then only run it
-    video_capture = cv2.VideoCapture(0)                                                                                                    # Selecting the Camera for Capturing video
-    while cv2.waitKey(1) != 113:                                                                                                           # Running the Program till ’q’ is pressed
-        ret, original_frame = video_capture.read()                                                                                         # ret -> return True if frame is successfully read, original_frame -> image given by the camera
-        if not ret:                                                                                                                        # check if getting the frame is successful or not
-            break                                                                                                                          # if not then break
-        gray_scale_frame = cv2.cvtColor(original_frame, cv2.COLOR_BGR2GRAY)                                                                # converting the frame to gray scale image
-        gray_scale_frame_blurred = cv2.blur(gray_scale_frame, (5, 5))                                                                      # blurring the image to get more accuracy and decrease load on cpu
-        balls = cv2.HoughCircles(gray_scale_frame_blurred, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=10, maxRadius=40)    # Detecting circles in the blurred image with some parameters set experimetally for a ball
-        if balls is not None:                                                                                                              # checks if there is a ball or not
-            balls = numpy.uint8(numpy.around(balls))                                                                                       # converts the float values in the balls matrix to integers (to be specific unsigned integer of 8 bits)
-            for ball in balls[0]:                                                                                                          # iterate through the detected balls
-                h, k, r = ball[0], ball[1], ball[2]                                                                                        # get the center (h, k) and radius r of the ball detected
-                cv2.circle(original_frame, (h, k), r, (0, 255, 0), 2)                                                                      # mark a circle with centre (h, k) and radius r to mark the detected ball
-                cv2.circle(original_frame, (h, k), 1, (0, 0, 255), 3)                                                                      # mark a point at the centre of the circle above
-        cv2.imshow("Ball Detection", original_frame)                                                                                       # Showing the image with marked balls if found
-    video_capture.release()                                                                                                                # Release the Video Capture
-    cv2.destroyAllWindows()                                                                                                                # Destroy all the OpenCV windows created by this program during its runtime
+sample_space = 5
+tolerence = 20
+
+def dist(c_1, c_2):
+    return math.sqrt((c_1[0]-c_2[0])**2+(c_1[1]-c_2[1])**2)
+def detectBalls(frames):
+    frames_circles = []
+    for frame in frames:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        hist = cv2.equalizeHist(gray)
+        blur = cv2.GaussianBlur(hist, (13, 13), cv2.BORDER_DEFAULT)
+        height, width = blur.shape[:2]
+        minR = round(width/40)
+        maxR = round(width/20)
+        minDis = round(width/7)
+        circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1, minDis, param1=14, param2=25, minRadius=minR, maxRadius=maxR)
+        if circles is not None:
+            circles = numpy.round(circles[0, :]).astype("int")
+            frame_circles = []
+            for (x, y, r) in circles:
+                frame_circles.append((x, y, r))
+            frames_circles.append(frame_circles)
+    if len(frames_circles) > 1:
+        reference_circles = frames_circles[1]
+    else:
+        return None
+    prev_min_dist, score = None, {i:0 for i in range(len(reference_circles))}
+    for circles in frames_circles[1:]:
+        index = None
+        for i, circle in enumerate(reference_circles):
+            dist_circles = {(circle, compare_circle): dist(circle, compare_circle) for compare_circle in circles}
+            min_dist = min(list(dist_circles.values()))
+            if min_dist > tolerence:
+                break
+            if prev_min_dist == None or min_dist < prev_min_dist:
+                prev_min_dist = min_dist
+                index = i
+        if index != None:
+            score[index] += 1
+    score = {j: score[j] for j in reversed(sorted(score, key=lambda i: score[i]))}
+    if len(score) > 0:
+        best_circle = list(score.keys())[0]
+        if score[best_circle] > 0:
+            return reference_circles[best_circle]
+        else:
+            return None
+    else:
+        return None
+
+if __name__ == "__main__":
+    video_capture = cv2.VideoCapture(0)
+    while cv2.waitKey(1) != 113:
+        frames = []
+        for _ in range(sample_space):
+            ret, frame = video_capture.read()
+            if not ret:
+                print("Error in Reading the Frame from the Camera")
+                continue
+            frames.append(frame)
+        ball = detectBalls(frames)
+        if ball != None:
+            cv2.circle(frames[0], (ball[0], ball[1]), ball[2], (0, 255, 0), 2)
+            cv2.rectangle(frames[0], (ball[0]-1, ball[1]-1), (ball[0]+1, ball[1]+1), (255, 0, 0), -1)
+        cv2.imshow("Ball Detection", frames[0])
+    video_capture.release()
+    cv2.destroyAllWindows()
